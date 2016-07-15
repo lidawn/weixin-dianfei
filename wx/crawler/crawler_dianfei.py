713813e30ddaa9505867cd3c4871bee0774656ee
lidawn <lidawn1991@gmail.com>
2016-07-15 15:18:44 +0800

    可以上线了

--- wx/crawler/crawler_dianfei.py
+++ wx/crawler/crawler_dianfei.py
@@ -4,6 +4,26 @@ import urllib,urllib2,re
 import params_post as pp
 import params_post_detail as ppd
 
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+user_agent = '''Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'''
+headers = {
+	'user-Agent':user_agent,
+	'connection':'keep-alive',
+	'Referer':'http://202.114.18.218/Main.aspx',
+	#'Referer':'http://www.cninfo.com.cn',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Encoding': 'gzip,deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4',
+	#原来是你的锅
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Host': '202.114.18.218',
+	'Origin': 'http://202.114.18.218'
+}
+
+
 AREAS = ['东区','西区','韵苑二期','韵苑一期','紫菘']
 VIEWSTATE = pp.pair_original['viewstate']
 EVENTVALIDATION = pp.pair_original['eventvalidation']
@@ -24,6 +44,8 @@ def crawler_twice():
 #根据post_detail一次查询
 def crawler_once(programId,txtyq,Txtroom):
 	res_data = {}
+	gou_dian_ming_xi = {}
+	chao_biao_ming_xi = []
 	url = 'http://202.114.18.218/Main.aspx'
 	values_3 = {'__EVENTTARGET':'',
 	'__EVENTARGUMENT':'',
@@ -34,10 +56,11 @@ def crawler_once(programId,txtyq,Txtroom):
 	'txtyq':'',#need
 	'txtld':'',#need
 	'Txtroom':'',#need
-	'ImageButton1.x':'16',
-	'ImageButton1.y':'12',
+	'ImageButton1.x':'35',
+	'ImageButton1.y':'8',
 	'TextBox2':'',
-	'TextBox3':''}
+	'TextBox3':''
+	}
 	i=0
 	values_3['programId'] = programId
 	values_3['txtyq'] = txtyq
@@ -45,32 +68,70 @@ def crawler_once(programId,txtyq,Txtroom):
 	values_3['txtld'] = Txtroom[0]+'层'
 
 	values_3['__VIEWSTATE'] =  ppd.pairs[programId][txtyq]['viewstate']
-	#values_3['__VIEWSTATE'] =  ppd.pairs[programId][txtyq]['viewstate']
 	values_3['__EVENTVALIDATION'] = ppd.pairs[programId][txtyq]['eventvalidation']
-	#获取电量
-	#层数大于规定时，urllib会出错
-	#层数对但是房间错，返回空值
-	data = urllib.urlencode(values_3)
-	req = urllib2.Request(url,data)
+	
 	try:
-		response = urllib2.urlopen(req)
-		the_page = response.read()
-		pattern = re.compile(r'name=\"TextBox2\"?.*value=\"(.*)\" readonly=',re.I)
-		zui_hou_chao_biao_shi_jian = pattern.findall(the_page)
-		#print zui_hou_chao_biao_shi_jian
-		res_data['zui_hou_chao_biao_shi_jian'] = zui_hou_chao_biao_shi_jian
-		pattern = re.compile(r'name=\"TextBox3\"?.*value=\"(.*)\" readonly=',re.I)
-		sheng_yu_dian_liang = pattern.findall(the_page)
-		res_data['sheng_yu_dian_liang'] = sheng_yu_dian_liang
-		#print sheng_yu_dian_liang
+		resp = requests.post(url,headers=headers,data=values_3)
+		content = BS(resp.content)
+		res_data['zui_hou_chao_biao_shi_jian'] = content.find('input',{'name':'TextBox2'}).get('value')
+		res_data['sheng_yu_dian_liang'] = content.find('input',{'name':'TextBox3'}).get('value')
 	except Exception,e:
 		print e
-	#['2015-7-26 7:09:09']
-	#['191.4']
-	#pattern = re.compile(r'<td>(.*)</td>',re.I)
-	#td_data = pattern.findall(the_page)
-	#chao_biao_shu_ju = td_data[5:12]
-	#gou_dian_ming_xi = td_data[12]
+	
 	return res_data
 
-print crawler_once('韵苑二期','韵苑15栋','1')
+#根据post_detail一次查询
+def crawler_detail(programId,txtyq,Txtroom):
+	res_data = {}
+	gou_dian_ming_xi = {}
+	chao_biao_ming_xi = []
+	url = 'http://202.114.18.218/Main.aspx'
+	values_3 = {'__EVENTTARGET':'',
+	'__EVENTARGUMENT':'',
+	'__LASTFOCUS':'',
+	'__VIEWSTATE':'',#need
+	'__EVENTVALIDATION':'',#need
+	'programId':'',#need
+	'txtyq':'',#need
+	'txtld':'',#need
+	'Txtroom':'',#need
+	'ImageButton1.x':'35',
+	'ImageButton1.y':'8',
+	'TextBox2':'',
+	'TextBox3':''
+	}
+	i=0
+	values_3['programId'] = programId
+	values_3['txtyq'] = txtyq
+	values_3['Txtroom'] = Txtroom
+	values_3['txtld'] = Txtroom[0]+'层'
+
+	values_3['__VIEWSTATE'] =  ppd.pairs[programId][txtyq]['viewstate']
+	values_3['__EVENTVALIDATION'] = ppd.pairs[programId][txtyq]['eventvalidation']
+	
+	try:
+		resp = requests.post(url,headers=headers,data=values_3)
+		content = BS(resp.content)
+		#gou_dian_ming_xi
+		gou_dian_ming_xi_tds = content.find('table',{'id':'GridView1'}).find_all('td')
+		gou_dian_ming_xi['chong_zhi'] = gou_dian_ming_xi_tds[0].string
+		gou_dian_ming_xi['dian_fei'] = gou_dian_ming_xi_tds[1].string
+		gou_dian_ming_xi['shi_jian'] = gou_dian_ming_xi_tds[2].string
+		res_data['gou_dian_ming_xi'] = gou_dian_ming_xi
+		#chao_biao_ming_xi
+		chao_biao_ming_xi_tds = content.find('table',{'id':'GridView2'}).find_all('td')
+		#print chao_biao_ming_xi_tds
+		for j in range(0,14,2):
+			chao_biao = {}
+			chao_biao['shu_ju'] = chao_biao_ming_xi_tds[j].string
+			chao_biao['shi_jian'] = chao_biao_ming_xi_tds[j+1].string
+			chao_biao_ming_xi.append(chao_biao)
+		res_data['chao_biao_ming_xi'] = chao_biao_ming_xi
+		#print res_data
+		#pattern = re.compile(r'name=\"TextBox2\"?.*value=\"(.*)\" readonly=',re.I)
+	except Exception,e:
+		print e
+
+	return res_data
+
+#print crawler_once('东区','沁苑东十舍','521')
--- wx/views.py
+++ wx/views.py
@@ -14,7 +14,7 @@ TOKEN = "dianfei521lidawn4"
 res_data = {}
 
 def help():
-	'''1.绑定寝室请回复: 绑定\n  绑定后直接回复任意内容查询寝室电费\n2.解除绑定请回复: 解除\n3.如找不到以上提示请回复: 帮助\n4.反馈建议请点击:<a href="http://form.mikecrm.com/BhpHew">反馈建议</a>
+	'''1.绑定寝室请回复: 绑定\n  绑定后回复任意内容查询电费\n2.解除绑定请回复: 解除\n3.提示本条消息请回复: 帮助\n4.反馈建议请点击：<a href="http://form.mikecrm.com/BhpHew">反馈建议</a>
     \n =====================\n
     如果没响应请多试几次，或者用校园网访问：<a href="http://202.114.18.218/Main.aspx">官方网站</a>'''
 
@@ -40,17 +40,20 @@ def intent(content):
 	#return 3 : 帮助  
 	#return 4 : 反馈
 	#return 5 : 任意字符
+	#return 6 : 详细
 	#print "content",content
 	intent = content.replace(' ','')
 	#print "intent",intent
 	if intent == '绑定':
 		return 1
-	elif intent == '解除':
+	elif intent == '解除' or intent == '解绑':
 		return 2
-	elif intent == '帮助':
+	elif intent == '帮助' or intent == 'help':
 		return 3
-	elif intent == '反馈':
+	elif intent == '反馈' or intent == '建议':
 		return 4
+	elif intent == '详细' or intent =='xx':
+		return 6
 	else :
 		return 5
 	
@@ -65,96 +68,110 @@ def handleRequest(request):
 	return msg
 		
 def responseMsg(request):
-    response = '剩余电量:'
-    from_weixin = handleRequest(request)
-    #关注
-    if from_weixin.get('MsgType') == 'event':
-    	#print 'Msgtype:event:'
-    	if from_weixin.get('Event') == 'subscribe':
-    		#print 'event:subscribe'
-    		content = help.__doc__
-    		response = content
-    	#扫码
-    	elif from_weixin.get('Event') == 'SCAN':
-    		#print 'event:scan'
-    		#TODO
-    		response = 'test'
-    #普通消息
-    else:
-    	weixin_id = from_weixin.get('FromUserName')
-    	if from_weixin.get('MsgType') == 'text':
-    		#print 'Msgtype:text:'
-    		text = from_weixin.get('Content') 
-    		#print 'text:',text
-    		i = intent(text)
-    		#print '判断:i',i
-    	else:
-    		i = 5
-    	#检查是否已绑定
-    	try:
-    		user = User.objects.get(weixin_id=weixin_id)
-    	except User.DoesNotExist:
-    		#用户不存在
-    		is_bounded = False
-    		is_user_exist = False
-    	else:
-    		is_bounded = user.is_bounded
-    		is_user_exist = True
+	response = '剩余电量:'
+	from_weixin = handleRequest(request)
+	#关注
+	if from_weixin.get('MsgType') == 'event':
+		#print 'Msgtype:event:'
+		if from_weixin.get('Event') == 'subscribe':
+			#print 'event:subscribe'
+			content = help.__doc__
+			response = content
+		#扫码
+		elif from_weixin.get('Event') == 'SCAN':
+			#print 'event:scan'
+			#TODO
+			response = 'test'
+	#普通消息
+	else:
+		weixin_id = from_weixin.get('FromUserName')
+		if from_weixin.get('MsgType') == 'text':
+			#print 'Msgtype:text:'
+			text = from_weixin.get('Content') 
+			#print 'text:',text
+			i = intent(text)
+			#print '判断:i',i
+		else:
+			i = 5
+		#检查是否已绑定
+		try:
+			user = User.objects.get(weixin_id=weixin_id)
+		except User.DoesNotExist:
+			#用户不存在
+			is_bounded = False
+			is_user_exist = False
+		else:
+			is_bounded = user.is_bounded
+			is_user_exist = True
+		if is_bounded:
+			#绑定用户，消息类型：1.绑定 2.解绑 3.帮助 4.反馈 5. 查自己 6.详细
+			area = user.quyu.encode("utf-8")
+			building = user.louhao.encode("utf-8")
+			room = str(user.fangjian)
+			if i == 1:
+				response = '您已绑定(%s),需要重新绑定请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>' \
+				% (area+building+room ,weixin_id)
+			elif i == 2:
+				#仅修改is_bounded
+				user.is_bounded = False
+				user.save()
+				response = '已帮您解绑(%s),需要重新绑定请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>' \
+				% (area+building+room ,weixin_id)
+			elif i == 3:
+				response = help.__doc__
+			elif i == 4:
+				#TODO
+				response = '反馈建议请点击<a href=\"http://form.mikecrm.com/BhpHew\">反馈建议</a>'
+			elif i == 5:
+				res_data = crawler_dianfei.crawler_once(area,building,room)
+				response = '您绑定的寝室号:%s\n===================\n剩余电量:%s\n最后抄表时间:%s\n===================\n详细抄表和购电请回复：详细\n如有疑问,请回复：帮助' \
+				%(area+building+room ,res_data.get('sheng_yu_dian_liang').encode('utf-8'),res_data.get('zui_hou_chao_biao_shi_jian').encode('utf-8'))
 
-    	if is_bounded:
-    		#绑定用户，消息类型：1.绑定 2.解绑 3.帮助 4.反馈 5. 查自己 
-    		area = user.quyu.encode("utf-8")
-    		building = user.louhao.encode("utf-8")
-    		room = str(user.fangjian)
-    		if i == 1:
-    			response = '您已绑定(%s),需要重新绑定请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>' \
-    			% (area+building+room ,weixin_id)
-    		elif i == 2:
-    			#仅修改is_bounded
-    			user.is_bounded = False
-    			user.save()
-    			response = '已帮您解绑(%s),需要重新绑定请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>' \
-    			% (area+building+room ,weixin_id)
-    		elif i == 3:
-    			response = help.__doc__
-    		elif i == 4:
-    			#TODO
-    			response = '反馈建议请点击<a href=\"http://form.mikecrm.com/BhpHew\">反馈建议</a>'
-    		elif i == 5:
-    			res_data = crawler_dianfei.crawler_once(area,building,room)
-    			response = '您绑定的寝室号：%s\n=============\n最后抄表时间：%s\n剩余电量:%s' \
-    			% (area+building+room,res_data.get('zui_hou_chao_biao_shi_jian')[0],res_data.get('sheng_yu_dian_liang')[0])
-    		
-    	else:
-    		#未绑定用户，消息类型：1.绑定 2.3.5. 帮助 4.反馈 
-    		if i == 1:
-    			#先存id，再返回链接
-    			if not is_user_exist:
-    				user = User(weixin_id=weixin_id,quyu="1",louhao="1",fangjian="1",is_bounded=False)
-    				user.save()
-    			response = '绑定寝室请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>'\
-    			% weixin_id
+			elif i == 6:
+				res_data = crawler_dianfei.crawler_detail(area,building,room)
+				li = res_data.get('chao_biao_ming_xi')
+				chao_biao_ming_xi = ''
+				for item in li:
+					chao_biao_ming_xi += item.get('shu_ju') + ' | ' + item.get('shi_jian')+'\n'
 
-    		elif i == 2 or i==3 or i==5 :
-    			response = help.__doc__
-    		elif i == 4:
-    			#TODO
-    			response = '反馈建议请点击<a href=\"\">反馈建议</a>'
-   
-    to_weixin  = '''<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content></xml>'''
-    to_weixin = to_weixin % (from_weixin['FromUserName'],from_weixin['ToUserName'],str(int(time.time())),'text',response)
-    #to_weixin = response
-    return to_weixin
-    
+				response = '''您绑定的寝室号：%s\n===================
+最近7天抄表：\n-------------------\n数据 | 时间\n-------------------\n%s===================
+最近购电明细：\n-------------------\n%s\n===================\n如有疑问，请输入：帮助''' \
+				% (area+building+room,
+				chao_biao_ming_xi.encode('utf-8'),
+				'电量: '+res_data.get('gou_dian_ming_xi').get('chong_zhi').encode('utf-8')\
+				+'\n电费: '+res_data.get('gou_dian_ming_xi').get('dian_fei').encode('utf-8')\
+				+'\n时间: '+res_data.get('gou_dian_ming_xi').get('shi_jian').encode('utf-8') )
+				
+		else:
+			#未绑定用户，消息类型：1.绑定 2.3.5.6 帮助 4.反馈 
+			if i == 1:
+				#先存id，再返回链接
+				if not is_user_exist:
+					user = User(weixin_id=weixin_id,quyu="1",louhao="1",fangjian="1",is_bounded=False)
+					user.save()
+				response = '绑定寝室请点击<a href=\"http://m1540057b2.iask.in/bound/?wid=%s\">绑定寝室</a>'\
+				% weixin_id
+			elif i == 2 or i==3 or i==5 or i==6:
+				response = help.__doc__
+			elif i == 4:
+				#TODO
+				response = '反馈建议请点击<a href=\"\">反馈建议</a>'
+	
+	to_weixin  = '''<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content></xml>'''
+	to_weixin = to_weixin % (from_weixin['FromUserName'],from_weixin['ToUserName'],str(int(time.time())),'text',response)
+	#to_weixin = response
+	return to_weixin
+
 @csrf_exempt   
 def check(request):
-    if request.method == 'GET':
-    	#验证
-        response = HttpResponse(checkSignature(request))
-        return response
-    elif request.method == 'POST':
-    	#接受微信消息
-        response = HttpResponse(responseMsg(request))
-        return response
-    else:
-        return None
+	if request.method == 'GET':
+		#验证
+		response = HttpResponse(checkSignature(request))
+		return response
+	elif request.method == 'POST':
+		#接受微信消息
+		response = HttpResponse(responseMsg(request))
+		return response
+	else:
+		return None
